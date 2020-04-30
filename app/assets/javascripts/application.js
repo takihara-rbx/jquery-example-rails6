61c7c324488361bcc51651342dea30d161a966c2
KodaiTakihara <takihara@rabbix.jp>
2020-04-30 09:28:24 +0900

    [add] first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,20 @@
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
+
+
+$(function(){
+  $('.page_name').css("color", '#FF0000')
+})
